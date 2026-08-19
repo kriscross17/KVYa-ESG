c4b685cfe716d30f47db3f38b47b7232d8e971d6
Krish Singh <krishsingh98906@gmail.com>
2026-08-20 02:06:14 +0530

    Update service worker cache version to v3

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'india-post-brsr-shell-v2';
+const CACHE = 'india-post-brsr-shell-v3';
 const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
